e2b5730326309db3ad64aee38cca7475a3a5d455
김태현 <cola9k@estsoft.com>
2023-01-14 17:19:52 +0900

    Revert "Loading, ErrorBoundary Suspense 처리"
    This reverts commit 5f70640c22d6528470819dd8bf3471120c6174b7.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import ErrorBoundary from 'components/common/ErrorBoundary';
-import Loading from 'components/common/Loading';
 import Header from 'components/layout/Header';
 import Main from 'components/layout/Main';
 import Login from 'components/Login/Login';
 import NotFound from 'components/NotFound/NotFound';
 import SignUp from 'components/SignUp/SignUp';
-
-import React, { lazy, Suspense } from 'react';
-import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
-
-const TodoList = lazy(() => import('components/Todo/TodoList/TodoList'));
-const Detail = lazy(() => import('components/Todo/Detail/Detail'));
+import Detail from 'components/Todo/Detail/Detail';
+import TodoList from 'components/Todo/TodoList/TodoList';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 	return (
@@ -20,30 +16,10 @@ function App() {
 				<Main>
 					<Routes>
 						<Route path="/" element={<div>빈 화면</div>} />
-						<Route path="login" element={<Login />} />
-						<Route path="signup" element={<SignUp />} />
-						<Route path="todo" element={<Outlet />}>
-							<Route
-								index
-								element={
-									<ErrorBoundary>
-										<Suspense fallback={<Loading />}>
-											<TodoList />
-										</Suspense>
-									</ErrorBoundary>
-								}
-							/>
-							<Route
-								path=":todoId"
-								element={
-									<ErrorBoundary>
-										<Suspense fallback={<Loading />}>
-											<Detail />
-										</Suspense>
-									</ErrorBoundary>
-								}
-							/>
-						</Route>
+						<Route path="/login" element={<Login />} />
+						<Route path="/signup" element={<SignUp />} />
+						<Route path="/todo" element={<TodoList />} />
+						<Route path="/todo/:id" element={<Detail />} />
 						<Route path="*" element={<NotFound />} />
 					</Routes>
 				</Main>
