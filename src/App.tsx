5f70640c22d6528470819dd8bf3471120c6174b7
김태현 <cola9k@estsoft.com>
2023-01-14 17:18:44 +0900

    Loading, ErrorBoundary Suspense 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import ErrorBoundary from 'components/common/ErrorBoundary';
+import Loading from 'components/common/Loading';
 import Header from 'components/layout/Header';
 import Main from 'components/layout/Main';
 import Login from 'components/Login/Login';
 import NotFound from 'components/NotFound/NotFound';
 import SignUp from 'components/SignUp/SignUp';
-import Detail from 'components/Todo/Detail/Detail';
-import TodoList from 'components/Todo/TodoList/TodoList';
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+
+const TodoList = lazy(() => import('components/Todo/TodoList/TodoList'));
+const Detail = lazy(() => import('components/Todo/Detail/Detail'));
 
 function App() {
 	return (
@@ -16,10 +20,30 @@ function App() {
 				<Main>
 					<Routes>
 						<Route path="/" element={<div>빈 화면</div>} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/signup" element={<SignUp />} />
-						<Route path="/todo" element={<TodoList />} />
-						<Route path="/todo/:id" element={<Detail />} />
+						<Route path="login" element={<Login />} />
+						<Route path="signup" element={<SignUp />} />
+						<Route path="todo" element={<Outlet />}>
+							<Route
+								index
+								element={
+									<ErrorBoundary>
+										<Suspense fallback={<Loading />}>
+											<TodoList />
+										</Suspense>
+									</ErrorBoundary>
+								}
+							/>
+							<Route
+								path=":todoId"
+								element={
+									<ErrorBoundary>
+										<Suspense fallback={<Loading />}>
+											<Detail />
+										</Suspense>
+									</ErrorBoundary>
+								}
+							/>
+						</Route>
 						<Route path="*" element={<NotFound />} />
 					</Routes>
 				</Main>
