62432732e8f62407f46ecdf787e6760b2e222aac
김태현 <cola9k@estsoft.com>
2023-01-15 17:43:56 +0900

    react-host-toast 추가

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import { globalStyles } from 'styles/global.style';
+import { Toaster } from 'react-hot-toast';
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -16,6 +17,7 @@ root.render(
 		<RecoilRoot>
 			<App />
 			<Alert />
+			<Toaster />
 		</RecoilRoot>
 	</>
 );
